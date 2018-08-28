9add74d7f6a82ccf50fa2983f4e9cd5beda29b93
blackhogwash <blackhogwash@gmail.com>
2018-08-28 21:47:11 +0300

    Added function for getting MD5-hash

--- filemonitoring/filemonitor.py
+++ filemonitoring/filemonitor.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 
 class FileMonitor:
@@ -11,13 +12,29 @@ class FileMonitor:
     def start(self):
         self.upd_files = os.listdir(self.path)
         if self.upd_files:
-            self.check()
+            self.check_dir()
+            self.check_file()
             self.files = self.upd_files
 
-    def check(self):
+    def check_dir(self):
         dif_files = list(set(self.files) - set(self.upd_files))
         add_files = list(set(self.upd_files) - set(self.files))
         if dif_files:
             print("Deleted: ", dif_files)
         if add_files:
             print("Added: ", add_files)
+
+    def get_file_hash(self, file_path):
+        if os.path.exists(file_path) is False:
+            return None
+        else:
+            md5 = hashlib.md5()
+            file = open(file_path)
+            for line in file:
+                md5.update(line)
+                file.close()
+            return md5.hexdigest()
+
+    def check_file(self):
+        for file in self.files:
+            print(self.get_file_hash(file))
